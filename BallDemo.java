908daa0e23815f4cb4d3f1a6ec8b1b73a3a35ba3
Sergio Acebes y Jorge Jaular <acebes9@gmail.com>
2018-02-23 08:58:28 +0000

    Ahora podemos crear la cantidad de bolas metidas por parametro

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,43 +12,44 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private BouncingBall[] balls;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+       
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int nBalls)
     {
         int ground = 400;   // position of the ground line
-
+        balls = new BouncingBall[nBalls];
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
-        // make them bounce
-        boolean finished =  false;
-        while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+        for(int i=1; i <= nBalls;i++){
+            BouncingBall ball = new BouncingBall(50 + (i*10), 70 + (i*10), 2+(4*i), Color.BLUE, ground, myCanvas);
+            ball.draw();
+            balls[i] = ball;
+        }
+        boolean move = true;
+        while(move!=true){
+            myCanvas.wait(50);
+            for(int j= 0;j < nBalls;j++){
+                balls[j].move();
+                if(balls[j].getXPosition() > 500){
+                    start=true;
+                }
             }
         }
+        
+        
     }
 }
